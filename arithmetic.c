6c5d9b2503920c22614ba895185a40a540a893db
Jayesh <jayeshpatil212000@gmail.com>
2022-06-19 00:09:52 +0530

    hello

---
+++ arithmetic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+   #include<conio.h>
+   int main()
+{
+    int a=5,b,c,d;
+    b=++a;
+    c=a++;
+    d=++a;
+    printf(" \n the value of a is :%d",a);
+    printf(" \n the value of b is :%d",b);
+    printf(" \n the value of c is :%d",c);
+    printf(" \n the value of d is :%d",d);
+    return 0;
+}
